7f08e0872e3e9fcc3eaf62015584276b5b2f3043
chunggouyeon <gouyeonch@dgu.ac.kr>
2023-08-13 04:09:48 +0900

    add thymeleaf logic

--- src/main/java/org/dongguk/sayoung/controller/IndexController.java
+++ src/main/java/org/dongguk/sayoung/controller/IndexController.java
@@ -2,17 +2,15 @@ package org.dongguk.sayoung.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.dongguk.sayoung.domain.Content;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @Controller
 @Slf4j
 @RequiredArgsConstructor
 public class IndexController {
-
+    @GetMapping("/")
+    public String indexController() {
+        return "index";
+    }
 }
